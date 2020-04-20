0b2ea09a6b694ac1f20a3b1a170f4cf33b6bdc2a
Stavros Filippidis <stavros489@hotmail.com>
2020-04-20 19:38:36 +0300

    Complete project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+import Recipe from "./Recipe";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+
+    const APP_ID = "b9819886";
+    const APP_KEY = "8f19ef083314ab1c2d87b506ed3c7d7b";
+
+    const [recipes, setRecipes] = useState([]) ; //empty array of obj
+    const [search, setSearch] = useState(('')) ;
+    const [query, setQuery] = useState('chicken');
+
+    useEffect(() => {                   //runs every time something changes
+        getRecipes();
+    }, [query]);                             // specify inside [] what changes , leave it empty to run only once
+
+    const getRecipes = async () => {
+        const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+        const data =await response.json();
+        setRecipes(data.hits);
+        console.log(data.hits);
+    };
+
+    const updateSearch = e => {
+        setSearch(e.target.value);
+        console.log(search);
+    };
+
+    const getSearch = e =>{
+        e.preventDefault();
+        setQuery(search);
+        setSearch(''); // set search text to empty after search
+
+    }
+
+    return (
+        <div className="App">
+            <form onSubmit={getSearch} className="search-form">
+                <input
+                    className="search-bar"
+                    type="text"
+                    value={search}
+                    onChange={updateSearch}
+                />
+                <button className="search-button" type="submit">Search</button>
+            </form>
+            <div className="recipes">
+                {recipes.map(recipe => (
+                <Recipe
+                    key={recipe.recipe.label}
+                    title={recipe.recipe.label}
+                    calories={recipe.recipe.calories}
+                    image={recipe.recipe.image}
+                    ingredients={recipe.recipe.ingredients}
+                />
+            ))}
+            </div>
+        </div>
+    )
+};
 
 export default App;
---
+++ src/Recipe.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import style from './recipe.module.css'
+
+const Recipe = ({title, calories, image, ingredients}) => {
+    return (
+        <div  className={style.recipe}>
+            <h1>{title}</h1>
+            <img  className={style.image} src={image}/>
+            <ol>
+                {ingredients.map(ingredient =>(
+                    <li>{ingredient.text}</li>
+                    )
+                )}
+            </ol>
+            <p>Calories: {calories}</p>
+        </div>
+    );
+};
+
+export default Recipe;
